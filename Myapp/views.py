8d2e6e34eb4635d902436f453750915bc267aecc
duningning <duning132@163.com>
2023-05-22 15:34:09 +0800

    完成端的改造-新端可创建时就生成对应模板
    后期因为要接入多个端，每个端下面都有需要添加的一些文件，所以

--- Myapp/views.py
+++ Myapp/views.py
@@ -1,4 +1,6 @@
 import datetime
+import os
+import shutil
 
 from django.shortcuts import render
 from Myapp.models import *
@@ -15,13 +17,22 @@ def home(request):
 
     return render(request,'home.html',res)# 后端用来把前端的home.html文件返回给浏览器
 
+# 新增端
 def save_platform(request):
     # 取到用户前端输入的值
     platform_name = request.GET["platform_name"]
     platform_host = request.GET["platform_host"]
     platform_monitor_time = request.GET["platform_monitor_time"]
     # 数据更新到数据库
-    DB_platform.objects.create(name=platform_name,host=platform_host,monitor_time=platform_monitor_time)
+    platform = DB_platform.objects.create(name=platform_name,host=platform_host,monitor_time=platform_monitor_time)
+    # 复制出自己的调试包
+    new_client_name = 'client_'+str(platform.id)
+
+    demo_path = 'MyClient/demo_client'
+    new_path = 'MyClient/'+new_client_name
+
+    shutil.copytree(demo_path,new_path)
+
 
     return HttpResponse('')
 
